1b786baeb94746634120b81517867f0af93349c1
Ossama Jouini <skymen@users.noreply.github.com>
2025-03-12 05:55:21 +0900

    Added tauri app

---
+++ package-tauri.js
@@ -0,0 +1,43 @@
+// move code from src to tauri-app/src
+
+const fs = require("fs");
+const path = require("path");
+
+const srcPath = path.join(__dirname, "src");
+const tauriAppPath = path.join(__dirname, "tauri-app/src");
+
+function removeDir(dir) {
+  if (fs.existsSync(dir)) {
+    fs.readdirSync(dir).forEach((file) => {
+      const curPath = path.join(dir, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        removeDir(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(dir);
+  }
+}
+
+function copyDir(src, dest) {
+  fs.mkdirSync(dest, { recursive: true });
+  fs.readdirSync(src).forEach((file) => {
+    const curPath = path.join(src, file);
+    const destPath = path.join(dest, file);
+    if (fs.lstatSync(curPath).isDirectory()) {
+      copyDir(curPath, destPath);
+    } else {
+      fs.copyFileSync(curPath, destPath);
+    }
+  });
+}
+
+removeDir(tauriAppPath);
+copyDir(srcPath, taurjsonaiAppPath);
+
+// remove service-worker.js from tauri-app/src
+fs.unlinkSync(path.join(tauriAppPath, "service-worker.js"));
+
+//empty content of service-worker-registration.js
+fs.writeFileSync(path.join(tauriAppPath, "service-worker-registration.js"), "");
